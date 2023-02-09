c212f3e618d98e5946d60f209cce4d7eb2b8ed16
chmh0805 <chmh0805@gmail.com>
2023-02-10 00:33:37 +0900

    Learn: Constant, Variable

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import (
-	"fmt"
-	mypackage "learngo/mypackage"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!");
-	mypackage.SayHello();
+	const name string = "hyuk";
+
+	/*
+		var_name := var_value
+			-> Go will infer the type of variable.
+			-> The type of value cannot be changed after been inferred.
+			-> This grammer cannot be used outside of function block.
+	*/
+	age := 27; // var age int16 = 27;
+
+	fmt.Printf("I'm %s. and %d years old.\n", name, age);
 }
