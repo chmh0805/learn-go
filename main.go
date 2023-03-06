b064bc4ec1a880a62dec524b7b4160d7e62fb50e
chmh0805 <chmh0805@gmail.com>
2023-03-06 23:36:23 +0900

    learn: make dictionary using map - 2

--- main.go
+++ main.go
@@ -10,10 +10,22 @@ func main() {
 		"first": "First Word",
 	}
 
-	definition, err := dictionary.Search("second");
+	word := "hello";
+	definition := "Greeting";
+	err := dictionary.Add(word, definition);
 	if err != nil {
 		fmt.Println(err);
+	}
+
+	def, err2 := dictionary.Search(word);
+	if err2 != nil {
+		fmt.Println(err2);
 	} else {
-		fmt.Println(definition);
+		fmt.Println(def);
+	}
+
+	err3 := dictionary.Add(word, definition);
+	if err3 != nil {
+		fmt.Println(err3);
 	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,6 +6,7 @@ import "errors"
 type Dictionary map[string]string // type alias
 
 var errNotFound = errors.New("not Found");
+var errAlreadyExist = errors.New("That word already Exists");
 
 // Search a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +15,16 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil;
 	}
 	return "", errNotFound;
+}
+
+// Add a word to dictionary
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word);
+	switch err {
+	case errNotFound:
+		d[word] = definition;
+	case nil:
+		return errAlreadyExist;
+	}
+	return nil;
 }
