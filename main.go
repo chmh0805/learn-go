1203bd47eb884b3539ac07459d23a18b10281d62
chmh0805 <chmh0805@gmail.com>
2023-03-08 00:14:48 +0900

    learn: goroutine - channel

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
-	go sexyCount("hyuk");
-	sexyCount("sun");
+	channel := make(chan bool); // make channel which has bool values
+	people := [2]string{"hyuk", "sun"};
+
+	for _, person := range people {
+		go isSexy(person, channel);
+	}
+
+	fmt.Println("Recieved this message: ", <- channel);
+	fmt.Println("Recieved this message: ", <- channel);
 }
 
-func sexyCount(person string) {
-	for i:=0; i<10; i++ {
-		fmt.Println(person, " is sexy", i);
-		time.Sleep(time.Second);
-	}
+func isSexy(person string, channel chan bool) {
+	time.Sleep(time.Second * 5)
+	channel <- true; // send value to channel.
 }
