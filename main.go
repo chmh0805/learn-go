f97829be438ae45705689cd79ba55e67177e27da
chmh0805 <chmh0805@gmail.com>
2023-03-23 22:14:35 +0900

    feat: JobScrapper-getPages

--- main.go
+++ main.go
@@ -1,55 +1,63 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct {
-	url string;
-	status string;
-	statusCode int;
-}
-
-var errRequestFailed = errors.New("Request Failed");
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python";
 
 func main() {
-	results := make(map[string]string);
-	c := make(chan requestResult);
-
-	urls := []string {
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	};
-
-	for _, url := range urls {
-		go hitURL(url, c);
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-c;
-		results[result.url] = result.status;
-	}
-
-	for url, status := range results {
-		fmt.Println(url, status);
+	totalPages := getPageCount();
+	
+	for i := 0; i < totalPages; i++ {
+		getPage(i);
 	}
 }
 
-/*
-	chan<- : send only
-*/
-func hitURL(url string, c chan<- requestResult) {
-	resp, err := http.Get(url);
-	status := "OK";
-	if err != nil || resp.StatusCode >= 400 {
-		status = "FAILED";
+func getPage(pageNum int) {
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(pageNum + 1);
+	fmt.Println("RequestURL: ", pageURL);
+}
+
+func getPageCount() int {
+	pageCount := 0;
+
+	req, reqErr := http.NewRequest("GET", baseURL, nil);
+	checkErr(reqErr);
+
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36");
+
+	client := &http.Client{}
+	res, err := client.Do(req);
+	checkErr(err);
+	checkCode(res);
+
+	defer res.Body.Close();
+
+	doc, err := goquery.NewDocumentFromReader(res.Body);
+	checkErr(err);
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pageCount = s.Find("a").Length();
+	})
+
+	return pageCount;
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err);
 	}
-	c <- requestResult{url: url, status: status, statusCode: resp.StatusCode}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode);
+	}
+	// log.Println("Request Status Code: ", res.StatusCode);
 }
